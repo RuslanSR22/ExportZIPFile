db32123c65524995671116fccf3e27dce1685e9f
Артем Максимович <tema.palamar4uck@yandex.ru>
2023-10-09 11:34:54 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Aspose.Zip;
+using Aspose.Zip.Saving;
+using CreateZIPFile1;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace ExportZIPFile
@@ -14,10 +15,30 @@ namespace ExportZIPFile
             // https://codebeautify.org/base64-to-image-converter тут можно посмотреть картинку
             var image = await GisService.GetImageAsync();
             // Путь до изображения
-            string pathImgFile = "";
+            string pathImgFile = SaveImage(image);
             ExportDXF.CreateDXF(pathImgFile);
 
             // Добавить метод сохранения зип файла
         }
-    }
+		private static string SaveImage(string sourceImage)
+		{
+			byte[] imageBytes = Convert.FromBase64String(sourceImage);
+			string jpgFilePath = @"D:\image.jpg"; //путь до изображения
+			File.WriteAllBytes(jpgFilePath, imageBytes);
+
+			string destination = @"D:\map.zip"; //путь до zip
+			using (FileStream zipFile = File.Open(destination, FileMode.Create)) {
+				// Файл для добавления в архив
+				using (FileStream source = File.Open(jpgFilePath, FileMode.Open, FileAccess.Read)) {
+					using (var archive = new Archive(new ArchiveEntrySettings())) {
+						// Добавить файл в архив
+						archive.CreateEntry("image.jpg", source);
+						// ZIP-файл
+						archive.Save(zipFile);
+					}
+				}
+			}
+			return jpgFilePath; // возвращаю путь на jpg image, а не на zip, потому что конструктор ImageDefinition не поддерживает такой формат
+		}
+	}
 }
